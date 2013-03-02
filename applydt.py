cea1480ab53f9290cfb22c4e56e3738452494121
Brendan Hasz <winsto99@gmail.com>
2013-03-01 22:03:55 -0500

    added applydt to apply the decision tree to a test file

---
+++ applydt.py
@@ -0,0 +1,15 @@
+# build_dt.py
+# Builds a decision tree from a file
+
+import sys
+from dt_io import *
+
+
+#Build decision tree from passed file
+apply_decision_tree(sys.argv[1], sys.argv[2])
+
+
+def apply_decision_tree(test_data_fn, dt_fn):
+    return filename
+
+
