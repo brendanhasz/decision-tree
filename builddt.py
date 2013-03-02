7ba5bd5bbb8d84217cde6d0da1dca6e84dbfde38
Brendan Hasz <winsto99@gmail.com>
2013-03-01 23:29:03 -0500

    added info_gain function

--- builddt.py
+++ builddt.py
@@ -28,12 +28,20 @@ def ID3(examples):
 
 
 def info_gain(Y, A):
+    thesum = 0
+    for e in set(A):
+        Ya = []
+        for i in range(0,len(A)):
+            if A[i]==e:
+                Ya.append(Y[i])
+        thesum = thesum + len(Ya)/len(A)*entropy(Ya)
+    return entropy(Y)-thesum
+        
     
 def entropy(Y):
     thesum = 0
-    yl=len(Y)
     for e in set(Y):
-        pyi = len([v for v in Y if v==e])/float(yl)
+        pyi = len([v for v in Y if v==e])/float(len(Y))
         thesum = thesum + pyi*log(1/pyi,2)
     return thesum
 
