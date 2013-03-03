ab7a5bdda3fde699a722f9b2ad4f303403413d79
Brendan Hasz <winsto99@gmail.com>
2013-03-02 22:43:29 -0500

    now passing example and attributes lists, just need to do ID3 to finish builddt

--- builddt.py
+++ builddt.py
@@ -7,23 +7,18 @@ from Tree import *
 from info_funcs import *
 
 
-#Build decision tree from passed file
-#build_decision_tree(sys.argv[1], sys.argv[2])
-
-
-def build_decision_tree(train_data_fn, dt_fn):
-    print "Building decision tree for "+train_data_fn+" ..."
-    examples = parse_training_file(train_data_fn) #Parse input
-    dt = ID3(exampes) #Run decision-tree building ID3 algorithm
-    save_dt(dt, dt_fn) #Save the decision tree
-    print "Built decision tree for "+train_data_fn+" saved in "+dt_fn
-
-
-def ID3(examples):
+def ID3(examples, attributes):
+    '''
+    Performs the Interactive Dichotomiser 3 algorithm on a list of examples
+    and a list of lists of attributes
+    '''
     root = Tree()
 
     #ID3 algorithm
  
+    print examples
+    print attributes[0]
+
     return root
 
 
@@ -32,4 +27,24 @@ def parse_training_file(filename):
     thefile = open(filename, 'r')
     [[data.append([l.strip().split(',')[0], l.strip().split(',')[1:]])] for l in thefile]
     thefile.close()
-    return data
+    examples = []
+    attributes = [[] for e in data[0][1]]
+    for e in data:
+        examples.append(e[0]) #example class
+        for i in range(0,len(e[1])):
+            attributes[i].append(e[1][i])            
+    return examples, attributes
+
+
+def build_decision_tree(train_data_fn, dt_fn):
+    print "Building decision tree for "+train_data_fn+" ..."
+    examples, attributes = parse_training_file(train_data_fn) #Parse input
+    dt = ID3(examples, attributes) #Run decision-tree building ID3 algorithm
+    save_dt(dt, dt_fn) #Save the decision tree
+    print "Built decision tree for "+train_data_fn+" saved in "+dt_fn
+
+
+#Build decision tree from passed file
+build_decision_tree(sys.argv[1], sys.argv[2])
+
+
--- dt_io.py
+++ dt_io.py
@@ -1,6 +1,7 @@
 # dt_io.py
 # Functions to save and load decision trees to file
 
+import pickle
 
 def save_dt(dt, filename):
     thefile = open(filename, 'w')
