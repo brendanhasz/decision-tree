f0709f9e78406e31ec200cac00a0cddc95b2f334
Brendan Hasz <winsto99@gmail.com>
2013-03-03 13:47:42 -0500

    fixed parse_test_file to take CSV files

--- applydt.py
+++ applydt.py
@@ -10,15 +10,17 @@ def classify(att, dt):
     '''
     Classify a single set of attributes, given a decision tree
     '''
+    return '1'
 
 
 def parse_test_file(filename):
     '''
-    Parses a test file into a list of lists of attributes
+    Parses a test file of comma-separated attributes into a list of
+     lists of attributes
     '''
     data = []
     thefile = open(filename, 'r')
-    [[data.append([c for c in l.strip()])] for l in thefile]
+    [data.append(l.strip().split(',')) for l in thefile]
     thefile.close()
     return data
 
@@ -37,13 +39,11 @@ def apply_decision_tree(test_data_fn, dt_fn, output_fn):
     print "Applying decision tree "+dt_fn+" to test file "+test_data_fn+" ..."
     att = parse_test_file(test_data_fn)
     dt = load_dt(dt_fn)
-    classes = []
-    for a in att:
-        classes.append(classify(a, dt))
+    classes = [classify(a, dt) for a in att]
     write_output_file(classes, att, output_fn)
     print "Done! Classified attributes stored in "+output_fn
 
 
 #Apply decision tree on data in passed file
-#apply_decision_tree(sys.argv[1], sys.argv[2], sys.argv[3])
+apply_decision_tree(sys.argv[1], sys.argv[2], sys.argv[3])
 
