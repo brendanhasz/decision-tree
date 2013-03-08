6b0fa244de287c5be2fe604cb3ed3d52832ed6e2
Brendan Hasz <winsto99@gmail.com>
2013-03-08 18:57:14 -0500

    split build and apply routines into separate files so we can include subroutines

--- bagdt.py
+++ bagdt.py
@@ -5,13 +5,15 @@ import sys
 from dt_io import *
 from Tree import *
 from random import randrange
-from builddt import *
+from builddt import ID3
+from builddt import parse_training_file
 
 def build_bagged_dt(train_fn, dt_fn, num_trees):
     print "Building Bootstrap Aggregate model for "+train_fn+" ..."
     examples, attributes = parse_training_file(train_fn) #parse input
     baglist = []
-    for i in range(1,int(num_trees)): #loop, each time
+    for k in range(0,int(num_trees)): #loop, each time
+        print "Building tree number "+str(k+1)+" of "+str(int(num_trees))
         ex_s = []
         att_s = [[] for e in attributes]
         for j in range(1,int(len(examples)*0.9)): #build sampled dataset
